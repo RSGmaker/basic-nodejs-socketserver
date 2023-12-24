bf46a98aa1bd0b5090dc85b712419351e59cbd62
RSGmaker <RSGamemaker@gmail.com>
2023-12-24 15:53:24 -0700

    Change port to 3000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express')
   , Iserver = require('socket.io')
   , io = Iserver(server);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 
 // routing
@@ -24,21 +24,6 @@ var roomdata = {};
 io.sockets.on('connection', function (socket) {
 	
 	// when the client emits 'adduser', this listens and executes
-	/*socket.on('adduser', function(username){
-		// store the username in the socket session for this client
-		socket.username = username;
-		// store the room name in the socket session for this client
-		socket.room = 'room1';
-		// add the client's username to the global list
-		usernames[username] = username;
-		// send client to room 1
-		socket.join('room1');
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, 'room1');
-	});*/
 	
 	socket.on('adduser', function(username,roomID){
 		if(typeof usernames[username] !== 'undefined'){
@@ -135,17 +120,6 @@ io.sockets.on('connection', function (socket) {
 		socket.emit('whoAmI',socket.username);
 	});
 	
-	/*socket.on('switchRoom', function(newroom){
-		socket.leave(socket.room);
-		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-		// update socket session room title
-		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-		socket.emit('updaterooms', rooms, newroom);
-	});*/
 	
 	function removeUser(socket){
 		// remove the username from global usernames list
@@ -161,9 +135,6 @@ io.sockets.on('connection', function (socket) {
 			}
 		}
 		// update list of users in chat, client-side
-		/*io.sockets.emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');*/
 		socket.leave(socket.room);
 	}
 	
