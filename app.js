64467577e45e8abb47501a202b1f8700a6277dfa
RSGmaker <RSGamemaker@gmail.com>
2023-12-24 20:57:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ var express = require('express')
 	  cors: {
     origin: "*",
     methods: ["GET", "POST"],
+	credentials: false
   },
   //, io = Iserver(server,{
   handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
             "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
+            "Access-Control-Allow-Credentials": false
         };
         res.writeHead(200, headers);
         res.end();
